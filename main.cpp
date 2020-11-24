73cc8f32392cd6fdb7b812eceaa750aa76b447ac
Omkar Kaul <omkar@xtra.co.nz>
2020-11-24 21:55:07 +1300

    added output_state function, fixed some -Wall compiler issues, added rotation 0 for both modes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 #include <string.h>
 #include "sha256.h"
 
+void output_state(std::string keyword, std::string data, std::string data_hash, 
+    std::string rotation_hash, int nonce, std::string target) {
+    // output the state of a successful mining rotation to stdout
+
+    std::cout << "DATA: " << data << std::endl;
+    std::cout << "DATA HASH: " << data_hash << std::endl;
+    std::cout << "ROTATION HASH: " << rotation_hash << std::endl;
+    std::cout << "NONCE: " << nonce << std::endl;
+    std::cout << "SOURCE: " << keyword << std::endl;
+    std::cout << "TARGET: " << target << std::endl;
+    std::cout << "-------------------------------\n" << std::endl;
+}
+
 bool is_prefix(std::string target, std::string data) {
     // checks if the target is a prefix of the data at index 0
     // i.e. the data "starts with" the target
@@ -17,28 +30,40 @@ std::string get_hash(std::string s) {
     return digest;
 }
 
-void mine(std::string keyword, std::string data, std::string target, std::string mode) {
+int mine(std::string keyword, std::string data, std::string target, std::string mode) {
     // performs the mining, mode 1 -> rotate, mode 2 -> fork
 
     // perform initial total hash
-    int nonce = 0;
+    std::cout << "ROTATION 0:" << std::endl;
+    int nonce = 1;
     std::string data_hash = get_hash(data);
     std::string concatenated_str = keyword + data_hash + std::to_string(nonce);
     std::string total_hash = get_hash(concatenated_str);
 
+    // perform first rotation of mining
+    while (!is_prefix(target, total_hash)) {
+        nonce += 1;
+        concatenated_str = keyword + data_hash + std::to_string(nonce);
+        total_hash = get_hash(concatenated_str);
+    }
+    
+    output_state(keyword, data, data_hash, total_hash, nonce, target);
+
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 4)
-        perror("Provide 4 args in order: {keyword} {data} {target} {mining mode}");
-        return 1;
+    if (argc <= 4) {
+        std::cout << "Provide 4 args: {keyword} {data} {target} {mining mode}" << std::endl;
+        return EXIT_FAILURE;
+    }
     
     std::string keyword = argv[1];
     std::string data = argv[2];
     std::string target = argv[3];
     std::string mode = argv[4];
 
-    mine(keyword, data, target, mode);
+    int val = mine(keyword, data, target, mode);
 
-    return 0;
+    return val;
 }
